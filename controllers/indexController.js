c64539b93def33b6a8d88ce7ff71ab01a961ba50
ali <wizard2070@gmail.com>
2023-08-10 15:14:36 +0430

    25th

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,7 +2,11 @@ import { asyncErrorRenderer } from "../middlewares/catchAsyncErrors.js";
 
 class IndexController {
     static home = asyncErrorRenderer(async (req, res, next) => {
-        res.render('home', { title: 'Home Page' })
+        const data = [
+            { name: 'ali', age: 30 },
+            { name: 'reza', age: 20 }
+        ];
+        res.render('home', { title: 'Home Page', data: JSON.stringify(data) })
     });
 
 }
---
+++ public/assets/js/scripts.js
@@ -0,0 +1,10 @@
+(function ($) {
+    "use strict";
+    $(window).on("load", function () { /*...*/ });
+})(jQuery);
+jQuery(document).ready(function ($) {
+    const data = JSON.parse($('#users_data').text());
+    console.log(data);
+
+});
+
