43f4a26dce49ddc4afebb9dd10c6e9672c4e6644
ali <wizard2070@gmail.com>
2024-09-06 10:18:36 +0330

    37th

--- app/setup.js
+++ app/setup.js
@@ -13,48 +13,65 @@ import favicon from "serve-favicon";
 import { engine } from "express-handlebars";
 import path from "path";
 import { fileURLToPath } from "url";
+import morgan from "morgan";
+import fs from "fs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 export default (app) => {
-    app.use(helmet());
-    app.use(bodyParser.json({ limit: '8mb' }));
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(express.static('public'));
-    app.use(favicon(path.join(__dirname, '../public/assets', 'favicon.ico')));
-    app.use(cookieParser());
-    app.use(fileUpload());
-    /** Sanitize Data */
-    app.use(mongoSanitize());
-    /** Prevent HTTP Parameter Pollution attacks */
-    app.use(hpp({
-        whitelist: ['positions']
-    }));
-    /** Rate Limit */
-    app.use(rateLimit({
-        windowMs: 10 * 60 * 1000, // 10 mins
-        max: 100
-    }));
-    /** Setup Cors */
-    app.use(cors());
-    /** Viewengine */
-    app.engine('handlebars', engine());
-    app.set('view engine', 'handlebars');
-    app.set('views', path.join(__dirname, '../views'));
-    /** Handle unhandled routes */
-    app.all('*', (req, res, next) => {
-        res.status(404).render('not_found', {
-            layout: false,
-            error: {
-                message: 'صفحه یافت نشد',
-                stack: '',
-            }
-        });
-        // next(new ErrorHandler(`${req.originalUrl} route not found`, 404));
+  app.use(bodyParser.json({ limit: "8mb" }));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  /** logging request info */
+  const ipFormat = global.isProd ? ":remote-addr" : "";
+  const format = `${ipFormat} :method :url :status :response-time ms :user-agent :date`;
+  const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, "../logs/access.log"),
+    { flags: "a" }
+  );
+  //   const reqLogger = morgan("combined");
+  const reqLogger = morgan(format, {
+    stream: accessLogStream,
+  });
+  app.use(reqLogger);
+  app.use(helmet());
+  app.use(express.static("public"));
+  app.use(favicon(path.join(__dirname, "../public/assets", "favicon.ico")));
+  app.use(cookieParser());
+  app.use(fileUpload());
+  /** Sanitize Data */
+  app.use(mongoSanitize());
+  /** Prevent HTTP Parameter Pollution attacks */
+  app.use(
+    hpp({
+      whitelist: ["positions"],
+    })
+  );
+  /** Rate Limit */
+  app.use(
+    rateLimit({
+      windowMs: 10 * 60 * 1000, // 10 mins
+      max: 100,
+    })
+  );
+  /** Setup Cors */
+  app.use(cors());
+  /** Viewengine */
+  app.engine("handlebars", engine());
+  app.set("view engine", "handlebars");
+  app.set("views", path.join(__dirname, "../views"));
+  /** Handle unhandled routes */
+  app.all("*", (req, res, next) => {
+    res.status(404).render("not_found", {
+      layout: false,
+      error: {
+        message: "صفحه یافت نشد",
+        stack: "",
+      },
     });
-    app.enable('view cache');
-    // app.get('/robots.txt', require('./routes/robots/index'));
-    /** Middleware to handle errors */
-    app.use(errorMiddleware);
-}
+    // next(new ErrorHandler(`${req.originalUrl} route not found`, 404));
+  });
+  app.enable("view cache");
+  // app.get('/robots.txt', require('./routes/robots/index'));
+  /** Middleware to handle errors */
+  app.use(errorMiddleware);
+};
